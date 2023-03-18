de5fe5ec8153d1eb631adff0cec6d23175002cda
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2023-03-19 03:36:16 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    
+    while(1)
+    {    
+        int a = 0, b =0;
+        cin >> a >> b;
+        if(a == 0 && b == 0) break;
+        if(a > b)
+        {
+            if(a%b == 0) cout << "multiple" << "\n";
+            else cout << "neither" << "\n";
+        }
+        else
+        {
+            if(b %a == 0 ) cout << "factor" << "\n";
+            else cout << "neither" << "\n";
+        }
+    }
+}

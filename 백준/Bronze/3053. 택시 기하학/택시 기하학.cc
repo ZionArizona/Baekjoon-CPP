09dfab3ee7ddd3a1dec959c6ba1950317ad931d4
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2023-03-19 03:35:37 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<map>
+#include<algorithm>
+
+#define PI 3.141592653589793238462;
+using namespace std;
+
+int main()
+{
+    double R;
+    cin >> R;
+    cout << fixed;
+    cout.precision(6);
+    cout << R*R*PI;
+    cout << endl;
+    cout << 2*R*R;
+   
+    
+
+    return 0;
+}

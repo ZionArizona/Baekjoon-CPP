0dd349c70c214d54a1c2b397b17c1b439c4645f6
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2023-07-20 23:31:08 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -13,9 +13,7 @@ int main()
     int a[n] = {0};
     for(int i = 0; i < n; i++) cin >> a[i];
     for(int i = n-1; i >= 0 ; i--){
-        if( (k/a[i]) != 0){
-            ans += (k/a[i]);
-        }
+        ans += k/a[i];
         k = k%a[i];
     }
     

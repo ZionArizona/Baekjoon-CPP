6693719ae0f6b56e67999c05e0b5abf4998d7e52
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2023-07-30 22:48:15 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,39 +1,36 @@
-#include <iostream>
+#include<iostream>
+#include<utility>
+#include<algorithm>
+#include<cmath>
 using namespace std;
 
+int d[1000001];
 
-int dp[1000001];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
-int main()
-{
-    int N;
-    cin >> N;
+
+    int n = 0;
+    cin >> n;
+
+    d[1] = 0;
     
-    dp[1] = 0;
-    dp[2] = 1;
-    dp[3] = 1;
-
-    if(N == 1){
-        cout << dp[1];
-    }
-    else if(N == 2){
-        cout << dp[2];
-    }
-    else if(N == 3){
-        cout << dp[3];
-    }
-    else{
-        for(int i = 4; i <= N; i++)
-        {
-            dp[i] = dp[i - 1] + 1;
-            if(i%3 == 0)
-                dp[i] = min(dp[i], dp[i/3] + 1);
-            if(i%2 == 0)
-                dp[i] = min(dp[i], dp[i/2] + 1);
-            
-        }  
-        cout << dp[N];
+    if(n  == 1){
+        cout << d[1];
+        return 0;
     }
     
-    return 0;
+    for(int i = 2; i <= n; i++){
+        d[i] = d[i-1] + 1;
+        if(i % 2 == 0){
+            d[i] = min(d[i/2] + 1, d[i]);
+        }
+        if(i % 3 == 0){
+            d[i] = min(d[i/3] + 1, d[i]);
+        }
+    }
+    
+    cout << d[n];
+    
 }

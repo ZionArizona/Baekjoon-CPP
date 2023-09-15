021870b559b82c518df604e71454931cbf7d1968
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2023-09-15 23:17:38 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,26 +1,12 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	
-	string str = "";
-	cin >> str;
-
-
-
-	for (int i = 0; i < str.length() / 2; i++) {
-		if (str[i] == str[str.length() - i - 1]) {
-			continue;
-		}
-		else {
-			cout << "0";
-			return 0;
-		}
-	}
-
-	cout << "1";
-
+#include<bits/stdc++.h>
+using namespace std;   
+string s, temp; 
+int main(){
+    cin >> s; 
+    temp = s; 
+    reverse(temp.begin(), temp.end()); 
+    if(temp == s) cout << 1 << "\n"; 
+    else cout << 0 << "\n"; 
+      
+    return 0; 
 }

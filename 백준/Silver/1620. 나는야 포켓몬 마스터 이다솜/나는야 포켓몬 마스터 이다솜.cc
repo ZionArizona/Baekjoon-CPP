3a58f736e0fa7363d0df35322ab4626ea6bf02dd
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2023-10-12 00:57:15 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 200 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,47 +1,24 @@
-#include<iostream>
-#include<map>
-#include<vector>
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-    
-    ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-    
-    int n, m;
-    map<string, int > mp;
-    string temp;
-    cin >> n  >> m;
-    vector<string> name;
-    vector<string> result;
-    
-    for(int i =1 ; i <=n ; i++)
-    {
-        cin >> temp;
-        mp.insert(make_pair(temp,i));
-        name.push_back(temp);
-    }
-    for(int i =0; i < m; i++)
-    {
-        cin >> temp;
-        if(temp[0] >= 65 && temp[0] <= 90)
-        {
-            result.push_back(to_string(mp[temp]));
-        }
-        else
-        {
-            result.push_back(name[stoi(temp) - 1]);
-        }
-    }
-    
-    
-    for(int i = 0; i < m; i++)
-    {
-        cout << result[i] << "\n";
-    }
-
-
-    return 0;
+int n, m;
+string s; 
+map<string, int> mp;
+map<int, string> mp2;
+int main(){  
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m; 
+	for(int i = 0; i < n; i++){
+		cin >> s; 
+		mp[s] = i + 1;  
+		mp2[i + 1] = s; 
+	}
+	for(int i = 0; i < m; i++){
+		cin >> s; 
+		if(s[0] >= 65 && s[0] <=90){
+			cout << mp[s] << "\n";
+		}else{
+			cout << mp2[stoi(s)] << "\n"; 
+		}
+	} 
 }

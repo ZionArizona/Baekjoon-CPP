d8b8c3672c02cbbd8f3b0c8670355d8ec0ad3e07
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2024-12-21 00:58:34 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 872 ms, Memory: 290180 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++){
+            String[] str = br.readLine().split(" ");
+            int A = Integer.parseInt(str[0]);
+            int B = Integer.parseInt(str[1]);
+            bw.write((A+B) + "\n");
+        }
+        
+        bw.flush();
+        br.close();
+        bw.close();
+        
+    }
+}

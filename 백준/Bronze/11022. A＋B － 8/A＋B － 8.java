5d5fd9c92a90d89290b06e6070fbe02b0234d584
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2024-12-21 01:07:08 +0900

    [Bronze V] Title: A+B - 8, Time: 144 ms, Memory: 16392 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++){
+            String[] str = br.readLine().split(" ");
+            int A = Integer.parseInt(str[0]);
+            int B = Integer.parseInt(str[1]);
+
+            System.out.println("Case #" + i +": " +A +" + " + B + " = " + (A+B) );
+        }
+
+    }
+}

76038faa8d2f3af03bdbc489e150868457a96f63
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2024-12-21 22:03:16 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char str = br.readLine().charAt(0);
+
+        System.out.println((int)str);
+
+    }
+}

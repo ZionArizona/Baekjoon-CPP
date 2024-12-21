2dec49191ac82c66cfe342f45fc51c6fe8801107
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2024-12-21 21:31:02 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N+1];
+
+        for(int i = 1 ; i<= N; i++) {
+            arr[i] = i;
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            while(start < end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        for(int i = 1; i <= N; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}

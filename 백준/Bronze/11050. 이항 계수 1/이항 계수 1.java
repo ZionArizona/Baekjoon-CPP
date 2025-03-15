bfd6b30a5cc6e1fc558b0a84000025178259fced
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2025-03-15 22:45:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,28 +1,29 @@
 import java.util.*;
 import java.io.*;
 
-public class Main
-{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());        
-        
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
-        System.out.println(coff(N,K));
-        
-    }
-    
-    
-    
-    public static int coff(int N, int K){
-        if(K == 0 || N == K){
-            return 1;
-        }
-        
-        return coff(N-1,K-1) + coff(N-1,K);
-    }
-    
-    
+public class Main {
+	
+	static int N,K;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		System.out.println(fac(N,K));
+		
+	} // main
+	
+	
+	static int fac(int N, int K) {
+		
+		if(K == 0 || N == K) {
+			return 1;
+		}
+		
+		return fac(N-1, K-1) + fac(N-1, K);
+	} // fac
+	
 }
